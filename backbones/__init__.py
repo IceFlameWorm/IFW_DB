4ca7beefe97cc5e34ef4a404b8aeec6e669e85da
linan <nan.li@learnable.ai>
2023-01-12 07:58:23 +0000

    ifw: add resnet modules modified with torchvision

--- backbones/__init__.py
+++ backbones/__init__.py
@@ -1,2 +1,7 @@
-from .resnet import resnet18, resnet34, resnet50, resnet101, deformable_resnet50, deformable_resnet18
+from .resnet import (
+    resnet18, resnet34, resnet50, resnet101, deformable_resnet50, deformable_resnet18
+)
+from .resnet_tv import (
+    resnet18_tv, resnet34_tv, resnet50_tv, resnet101_tv, deformable_resnet18_tv, deformable_resnet50_tv
+)
 from .mobilenetv3 import mobilenet_v3_large, mobilenet_v3_small
