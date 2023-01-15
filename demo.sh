46ba1b2e9e9e092b490aa4a50e9eff422e6ca03b
linan <nan.li@learnable.ai>
2023-01-15 13:35:40 +0000

    add scripts for evaluation, and modify the demo.sh

--- demo.sh
+++ demo.sh
@@ -1,6 +1,6 @@
-python demo.py experiments/seg_detector/totaltext_resnet18_deform_thre.yaml \
-    --image_path datasets/total_text/train_images/img107.jpg  \
-    --resume ~/Projects/IFW_DB/outputs/workspace/IFW_DB/SegDetectorModel-seg_detector/deformable_resnet18_tv/L1BalanceCELoss/model/model_epoch_0_minibatch_0 \
+python demo.py experiments/ASF/diff_resnet50_deform_thre_asf.yaml \
+    --image_path datasets/total_text_diff/train_images/0a0d922d-b2c9-419f-8c58-c7d58466c975.jpg \
+    --resume /home/agent_ln/model_files/test_train/model_epoch_38_minibatch_75000 \
     --polygon \
-    --box_thresh 0.7 \
+    --box_thresh 0.5 \
     --visualize
---
+++ eval_2.sh
@@ -0,0 +1,4 @@
+python eval_2.py \
+    experiments/ASF/diff_resnet50_deform_thre_asf.yaml \
+    --resume /home/agent_ln/model_files/test_train/model_epoch_38_minibatch_75000 \
+    --polygon --box_thresh 0.5
